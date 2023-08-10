00a22d6513508e6f8abb0ae2a82d182f45068a1f
ahernd2 <david.ahern@dell.com>
2023-08-10 09:35:31 +0100

    Updated

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -6,32 +6,32 @@ export const reducer = createReducer(
   initialState,
   on(actions.accepted, (state, { date }) => ({
       ...state,
-    dates: {[date]: { ...state.dates[date], accepted: (state.dates[date]?.accepted ?? 0) + 1 }}
+    dates: {...state.dates, [date]: { ...state.dates[date], accepted: (state.dates[date]?.accepted ?? 0) + 1 }}
   })),
   on(actions.acceptedModified, (state, { date }) => ({
     ...state,
-    dates: {[date]: {
+    dates: {...state.dates, [date]: {
       ...state.dates[date],
       acceptedModified: (state.dates[date]?.acceptedModified ?? 0) + 1
     }}
   })),
   on(actions.didNotAccept, (state, { date }) => ({
     ...state,
-    dates: {[date]: { ...state.dates[date], rejected: (state.dates[date]?.rejected ?? 0) + 1 }}
+    dates: {...state.dates, [date]: { ...state.dates[date], rejected: (state.dates[date]?.rejected ?? 0) + 1 }}
   })),
   on(actions.complicated, (state, { date }) => ({
     ...state,
-    dates: {[date]: {
+    dates: {...state.dates, [date]: {
       ...state.dates[date],
       complicatedAlgorithm: (state.dates[date]?.complicatedAlgorithm ?? 0) + 1
     }}
   })),
   on(actions.positiveBoost, (state, { date, size }) => ({
     ...state,
-    dates: {[date]: { ...state.dates[date], timeSavedEstimate: (state.dates[date]?.timeSavedEstimate ?? 0) + size }}
+    dates: {...state.dates, [date]: { ...state.dates[date], timeSavedEstimate: (state.dates[date]?.timeSavedEstimate ?? 0) + size }}
   })),
   on(actions.negativeBoost, (state, { date, size }) => ({
     ...state,
-    dates: {[date]: { ...state.dates[date], timeLostEstimate: (state.dates[date]?.timeLostEstimate ?? 0) + size }}
+    dates: {...state.dates, [date]: { ...state.dates[date], timeLostEstimate: (state.dates[date]?.timeLostEstimate ?? 0) + size }}
   })),
 );
